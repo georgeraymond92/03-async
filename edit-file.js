f46bde0a492188f90a8b5afe94ff2a897bfa1bc5
george <geomarray@gmail.com>
2018-12-20 08:49:36 -0800

    PR for submit

--- edit-file.js
+++ edit-file.js
@@ -1,19 +1,20 @@
+'use strict';
 const fs = require('fs');
 const faker =require('faker');
 
 let file = './files/' + process.argv[2];
 
 const randomNumber = faker.random.number();
-console.log(randomNumber);
+
 
 fs.readFile(file,(err,data) => {
-    if(err) throw err;
-    console.log(data.toString());
-    fs.appendFile(file, randomNumber, (err) =>{
-        if (err) throw err
-        fs.readFile(file,(err,data) => {
-            if(err) throw err;
-            console.log(data.toString());
-        });
+  if(err) throw err;
+  console.log(data.toString());
+  fs.appendFile(file, randomNumber, (err) =>{
+    if (err) throw err;
+    fs.readFile(file,(err,data) => {
+      if(err) throw err;
+      console.log(data.toString());
     });
+  });
 });
